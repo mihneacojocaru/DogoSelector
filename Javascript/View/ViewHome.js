62f6b86e625ec722a47f7f12961f49ced5906cb8
mihneacojocaru <mihnea.co@gmail.com>
2021-11-17 17:40:08 +0100

    bug fixes

--- Javascript/View/ViewHome.js
+++ Javascript/View/ViewHome.js
@@ -6,8 +6,15 @@ export default class ViewHome {
     this.root.innerHTML += this.printHeader();
     this.root.innerHTML += this.printMain();
     this.controller = new ControllerDogApi();
-    this.addSelectItems();
-    this.getNewDog();
+
+    
+    //this.addSelectItems();
+    this.allBreeds();
+    //this.setNewDog();
+    this.select = document.querySelector("select");
+    this.select.addEventListener("change",this.handleChange);
+    this.anotherOne();
+
   }
 
   printHeader = () => {
@@ -24,42 +31,88 @@ export default class ViewHome {
             <select></select>
             <div class="pic-container">
                 <img src="https://images.dog.ceo/breeds/affenpinscher/n02110627_1559.jpg" alt="dog chilling on the porch">
-            </div>              
+            </div>  
+            <button id="btn">Another one!</button>            
         </div>
     </main>
         `;
   };
 
-  addSelectItems = () => {
+  addSelectItems = lista => {
     let select = document.querySelector("select");
+    
+    for(const e in lista){
+      select.innerHTML += `<option value="${e}">${e}</option>`;
+    }
+  }
 
-    setTimeout(() => {
-      this.controller.list.forEach((e) => {
-        select.innerHTML += `<option value="${e}">${e}</option>`;
-      });
-    }, 200);
-  };
+  allBreeds = async () => {
+    try {
+        const link = "https://dog.ceo/api/breeds/list/all";
 
-  getNewDog = async () => {
+    let data = await fetch(link);
+
+    let d = await data.json();
+
+    d = await d.message;
+
+    this.addSelectItems(d);
+
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  setNewDog = async () => {
     try {
       const select = document.querySelector("select");
-      const picContainer = document.querySelector(".pic-container");
 
-      const link = await fetch(`https://dog.ceo/api/breed/affenpinscher/images/random`);
-      let data = await link.json();
-      let d = await data.message;
-      picContainer.innerHTML = `<img src="${d}" alt="dog chilling">`;
+      select.addEventListener("change", () => {
+        this.getNewDog();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-      select.addEventListener("change", async () => {
+  handleChange=(e)=>{
+
+
+    e.preventDefault();
+
+    let obj=e.target;
+
+
+    let url=`https://dog.ceo/api/breed/${obj.value}/images/random`;
+
+
+    fetch(url)
+    .then(data=>data.json())
+    .then(data=>data.message)
+    .then(this.setFirstDog);
+
+
+  }
+
+  getNewDog = async () => {
+        const select = document.querySelector("select");
         const link = await fetch(`https://dog.ceo/api/breed/${select.value}/images/random`);
         let data = await link.json();
         let d = await data.message;
 
         let picContainer = document.querySelector(".pic-container");
         picContainer.innerHTML = `<img src="${d}" alt="dog chilling">`;
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  }
+
+  setFirstDog = (url) => {
+    const picContainer = document.querySelector(".pic-container");
+
+    picContainer.innerHTML = `<img src="${url}" alt="dog chilling">`;
+  }
+
+  anotherOne = () => {
+    let btn = document.querySelector("#btn");
+    btn.addEventListener("click",this.getNewDog);
+  }
+
 }
